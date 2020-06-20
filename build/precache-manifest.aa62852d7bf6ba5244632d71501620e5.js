491f26fcc01ab79db60cf5d986c9ffcae51b861e
Alon-Fabio <alon.the.fabio@gmail.com>
2020-06-20 10:48:59 +0300

    React app publish it to GitHub Pages

---
+++ build/precache-manifest.aa62852d7bf6ba5244632d71501620e5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42a0e31494bc62465ee8bf0f8ab6093e",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "1f2b159e6f44ef7a6137",
+    "url": "/RoboFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "02e204586e5c03ad98a4",
+    "url": "/RoboFriends/static/css/main.7ff0d6bc.chunk.css"
+  },
+  {
+    "revision": "1f2b159e6f44ef7a6137",
+    "url": "/RoboFriends/static/js/2.401976fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RoboFriends/static/js/2.401976fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02e204586e5c03ad98a4",
+    "url": "/RoboFriends/static/js/main.d5ceb79b.chunk.js"
+  },
+  {
+    "revision": "5aeb8e7bc9cd8564f66a",
+    "url": "/RoboFriends/static/js/runtime-main.93a75372.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
