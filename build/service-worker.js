dc5cff88acb9a745fe36c17cc9f2c158f3d87bed
Alon-Fabio <alon.the.fabio@gmail.com>
2020-08-01 10:02:47 +0300

    App manifest

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/robofriends/precache-manifest.4d608d2aec701b470b2c7f0d28a01124.js"
+  "/robofriends/precache-manifest.cf44f692dbef43a8ef5a042307b7b118.js"
 );
 
 self.addEventListener('message', (event) => {
