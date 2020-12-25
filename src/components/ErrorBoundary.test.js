81b36a6dcf5bb107a4ae9b728035dbc269d4b730
Alon-Fabio <alon.the.fabio@gmail.com>
2020-12-25 21:24:07 +0200

    CircleCi added

--- src/components/ErrorBoundary.test.js
+++ src/components/ErrorBoundary.test.js
@@ -1,46 +1,50 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import ErrorBoundary from './ErrorBoundary';
-import {shallow, mount} from 'enzyme';
- 
-let wrapper
-let wrapperShallow
-let wrapperMountWithError
-beforeEach(()=>{
-    const TestComponent = () =>{
-        return <div>"Testing"</div>
-    }
+import React from "react";
+import renderer from "react-test-renderer";
+import ErrorBoundary from "./ErrorBoundary";
+import { shallow, mount } from "enzyme";
 
-    const ComponentWithError = () =>{
-        const error = new Error('This is a test error!');
-        return <h1>{error}</h1>
-    }
+let wrapper;
+let wrapperShallow;
+let wrapperMountWithError;
+beforeEach(() => {
+  const TestComponent = () => {
+    return <div>"Testing"</div>;
+  };
 
-    wrapper = renderer.create(
-        <ErrorBoundary > 
-            <TestComponent />
-        </ErrorBoundary>    
-    ).toJSON()
+  const ComponentWithError = () => {
+    const error = new Error("This is a test error!");
+    return <h1>{error}</h1>;
+  };
 
-    wrapperShallow = shallow(
-        <ErrorBoundary > 
-            <TestComponent />
-        </ErrorBoundary> 
+  wrapper = renderer
+    .create(
+      <ErrorBoundary>
+        <TestComponent />
+      </ErrorBoundary>
     )
-    // Will print out the error, but is a good test for production.
-    wrapperMountWithError = mount(
-        <ErrorBoundary > 
-            <ComponentWithError />
-        </ErrorBoundary >    
-    )
-})
+    .toJSON();
 
-describe("Testing ErroeBoundary", ()=>{
-    it("Snapshot with TestComponent", ()=>{
-        expect(wrapper).toMatchSnapshot()
-    })
-    // Will print out the error, but is a good test for production.
-    it("Error in component", ()=>{
-        expect(wrapperMountWithError.find('h1').text()).toEqual("Opsss.. this souldnt have happend")
-    })
-})
+  wrapperShallow = shallow(
+    <ErrorBoundary>
+      <TestComponent />
+    </ErrorBoundary>
+  );
+  // Will print out the error, but is a good test for production.
+  wrapperMountWithError = mount(
+    <ErrorBoundary>
+      <ComponentWithError />
+    </ErrorBoundary>
+  );
+});
+
+describe("Testing ErroeBoundary", () => {
+  it("Snapshot with TestComponent", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+  // Will print out the error, but is a good test for production.
+  it("Error in component", () => {
+    expect(wrapperMountWithError.find("h1").text()).toEqual(
+      "Opsss.. this souldnt have happend"
+    );
+  });
+});
